84a8f9bee9049051e78d37d387ca69cccffe3e10
Andrii Danylenko <62876824+Andrii-Danylenko@users.noreply.github.com>
2024-11-05 15:34:09 +0200

    Fixed UserController.java
    Added missed comma

--- core/src/main/java/greencity/controller/UserController.java
+++ core/src/main/java/greencity/controller/UserController.java
@@ -661,7 +661,7 @@ public class UserController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
             @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
-            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED)
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
             @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN)
     })
     @PutMapping("/language/{languageId}")
